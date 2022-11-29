5d3095bb7404356956ff145981a197ad42215d41
Ammar Tahir <ammartahir1524@gmail.com>
2022-11-28 22:02:49 -0500

    see simulate usage in dram.py and main.py

--- src/dram.py
+++ src/dram.py
@@ -4,25 +4,27 @@ dram operations
 
 class DRAM():
 	"""DRAM simulation based on DDR4"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self, clock):
+		self.clock = clock
 
-	def activate(bank, row):
+	def activate(self, bank, row):
 		''' opens a row: transfers a row from bank to its row buffer'''
+		print(bank, row)
+		self.clock.schedule(self.read, (bank, 2), run_time=1)
 		return
 
-	def read(bank, column):
+	def read(self, bank, column):
 		''' read column from bank's row buffer'''
 		value = None
 		return value
 
-	def write(bank, column, value):
+	def write(self, bank, column, value):
 		''' write value to column of row bank'''
 		return
 
-	def precharge(bank):
+	def precharge(self, bank):
 		''' wrtie back row buffer to appropriate row of the bank'''
 		return
 
-	def refresh(row):
+	def refresh(self, row):
 		''' refresh all bank's row by activating and precharging it'''
