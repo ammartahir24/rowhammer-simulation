1c8bb4c834e4eb6b4c3df02d4da8e02195ae5c1b
Meghna Mandava <meghnam4@illinois.edu>
2022-12-01 23:08:01 -0600

    simulation taking forever

--- src/main.py
+++ src/main.py
@@ -3,40 +3,55 @@ from simulate import Clock
 from dram import DRAM
 
 clock = Clock()
-memory = DRAM(clock, 2, 10, 10)
+memory = DRAM(clock, 2, 30, 30)
 
 # start events here e.g. rowhammer code execution or row activation by queuing smth in clock
 #clock.schedule(memory.activate, (1,1), run_time=10)
 time = 0
-for j in range(10):
+for j in range(30):
 	time += cfg.activation_time
-	print(time, "activate", j)
+	#print(time, "activate", j)
 	clock.schedule(memory.activate, (1,j), run_time=time)
-	for i in range(10):
+	for i in range(30):
 		time += cfg.write_time
-		print(time, "write", i)
+		#print(time, "write", i)
 		clock.schedule(memory.write, (1, i, 1), run_time=time)
 	time += cfg.precharge_time
-	print(time, "precharge", j)
+	#print(time, "precharge", j)
 	clock.schedule(memory.precharge, (1), run_time=time)
 
+"""
 time += .05 * (10**9)
 for j in range(10):
 	time+= cfg.activation_time+cfg.precharge_time
 	clock.schedule(memory.refresh, (j), run_time=time)
 time += .05 * (10**9)
+"""
 
+for i in range(50000): 
+	#print(i)
+	for j in range(10,20):
+		time += cfg.activation_time
+		#print(time, "activate", j)
+		clock.schedule(memory.activate, (1,j), run_time=time)
+		for i in range(30):
+			time += cfg.write_time
+			#print(time, "write", i)
+			clock.schedule(memory.write, (1, i, 1), run_time=time)
+		time += cfg.precharge_time
+		#print(time, "precharge", j)
+		clock.schedule(memory.precharge, (1), run_time=time)
 
-for j in range(10):
+for j in range(30):
 	time += cfg.activation_time
-	print(time, "activate", j)
+	#print(time, "activate", j)
 	clock.schedule(memory.activate, (1,j), run_time=time)
-	for i in range(10):
+	for i in range(30):
 		time += cfg.read_time
-		print(time, "read", i)
+		#print(time, "read", i)
 		clock.schedule(memory.read, (1, i), run_time=time)
 	time += cfg.precharge_time
-	print(time, "precharge", j)
+	#print(time, "precharge", j)
 	clock.schedule(memory.precharge, (1), run_time=time)
 
 simulation_time = 200000000
