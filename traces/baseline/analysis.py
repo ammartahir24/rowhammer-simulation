4914c409c15595972abb484a24cddb49456da44a
Ammar Tahir <ammartahir1524@gmail.com>
2022-12-09 03:28:51 -0500

    counting script

---
+++ traces/baseline/analysis.py
@@ -0,0 +1,20 @@
+file = open("baseline2.log", "r")
+contents = file.read().split("\n")
+contents = [l.split(" ") for l in contents]
+contents = [(int(l[3],0), int(l[4],0)) for l in contents]
+
+contents = sorted(contents, key=lambda x:x[0])
+
+def bitExtracted(number, k, p):
+	return ( ((1 << k) - 1)  &  (number >> (p-1) ) )
+
+# for c in contents:
+# 	print(c)
+
+for i in range(int(len(contents)/8)):
+	bitflips = 0
+	for j in range(8):
+		for k in range(8):
+			if bitExtracted(contents[i*8+j][1], 1, k+1) == 0:
+				bitflips += 1
+	print(bitflips)
